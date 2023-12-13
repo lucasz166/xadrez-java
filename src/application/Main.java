6da004fef18e72d505846f83284b382dae6f7ccf
Lucas Gabriel <lucasgabriel@Lucass-MacBook-Air.local>
2023-12-13 20:15:30 -0300

    Projeto jogo de xadrez em javat

---
+++ src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import chess.*;
+import java.util.ArrayList;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
+
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printMatch(chessMatch, captured);
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
+            } catch (ChessException e) {
+                System.out.println(e.getClass());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getClass());
+                sc.nextLine();
+            }
+
+        }
+    }
+}
